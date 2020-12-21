5659d30d403d6bd86b252a8fcd5ad9cadd61b852
Suraj <surajshrm000@gmail.com>
2020-12-21 20:07:40 +0530

    [Suraj] Add: Creating the Contact

---
+++ AddressBookProblem/IContacts.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBookProblem
+{
+    interface IContacts
+    {
+        public void AddContact(string firstName, string lastName, string address, string city, string state, string email, int zip, long phoneNumber);
+    }
+}
---
+++ AddressBookProblem/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AddressBookProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book Program");
+            AddressBook addressBook = new AddressBook();
+            addressBook.AddContact("Rishabh", "Pant", "Uttarakhand", "Moti Bagh", "UK", "rishabh@gmail.com", 499945, 7654378654);
+            addressBook.ViewContact();
+        }
+    }
+}
