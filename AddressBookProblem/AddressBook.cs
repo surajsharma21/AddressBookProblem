b4395436cf6133e4ae59e5ae64e9f4ec4175c164
Suraj <surajshrm000@gmail.com>
2020-12-21 20:21:16 +0530

    [Suraj] Add: Delete Contact using name

--- AddressBookProblem/AddressBook.cs
+++ AddressBookProblem/AddressBook.cs
@@ -19,6 +19,24 @@ namespace AddressBookProblem
             contact.Zip = zip;
             contact.PhoneNumber = phoneNumber;
             addressBook.Add(contact.FirstName, contact);
+            Console.WriteLine("\nAdded Succesfully. \n");
+        }
+        public void ViewContact(string name)
+        {
+            foreach (KeyValuePair<string, Contact> item in addressBook)
+            {
+                if (item.Key.Equals(name))
+                {
+                    Console.WriteLine("First Name : " + item.Value.FirstName);
+                    Console.WriteLine("Last Name : " + item.Value.LastName);
+                    Console.WriteLine("Address : " + item.Value.Address);
+                    Console.WriteLine("City : " + item.Value.City);
+                    Console.WriteLine("State : " + item.Value.State);
+                    Console.WriteLine("Email : " + item.Value.Email);
+                    Console.WriteLine("Zip : " + item.Value.Zip);
+                    Console.WriteLine("Phone Number : " + item.Value.PhoneNumber + "\n");
+                }
+            }
         }
         public void ViewContact()
         {
@@ -77,7 +95,20 @@ namespace AddressBookProblem
                             item.Value.PhoneNumber = Convert.ToInt64(Console.ReadLine());
                             break;
                     }
+                    Console.WriteLine("\nEdited Successfully.\n");
                 }
+            }
+        }
+        public void DeleteContact(string name)
+        {
+            if (addressBook.ContainsKey(name))
+            {
+                addressBook.Remove(name);
+                Console.WriteLine("\nDeleted Succesfully.\n");
+            }
+            else
+            {
+                Console.WriteLine("\nNot Found, Try Again.\n");
             }
         }
     }
--- AddressBookProblem/IContacts.cs
+++ AddressBookProblem/IContacts.cs
@@ -7,6 +7,8 @@ namespace AddressBookProblem
     interface IContacts
     {
         public void AddContact(string firstName, string lastName, string address, string city, string state, string email, int zip, long phoneNumber);
+        public void ViewContact(string name);
         public void EditContact(string name);
+        public void DeleteContact(string name);
     }
 }
