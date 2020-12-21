889dac54430caf1c8c6aa1384c4085f04ed4ac0f
Suraj <surajshrm000@gmail.com>
2020-12-21 20:16:40 +0530

    [Suraj] Add: Editing Existing Contact

--- AddressBookProblem/IContacts.cs
+++ AddressBookProblem/IContacts.cs
@@ -7,5 +7,6 @@ namespace AddressBookProblem
     interface IContacts
     {
         public void AddContact(string firstName, string lastName, string address, string city, string state, string email, int zip, long phoneNumber);
+        public void EditContact(string name);
     }
 }
