ba563d51e92ec09737c126e84b401d39e10b8f90
Suraj <surajshrm000@gmail.com>
2020-12-21 20:12:29 +0530

    [Suraj] Add: Adding contact through console

--- AddressBookProblem/Program.cs
+++ AddressBookProblem/Program.cs
@@ -8,7 +8,23 @@ namespace AddressBookProblem
         {
             Console.WriteLine("Welcome to Address Book Program");
             AddressBook addressBook = new AddressBook();
-            addressBook.AddContact("Rishabh", "Pant", "Uttarakhand", "Moti Bagh", "UK", "rishabh@gmail.com", 499945, 7654378654);
+            Console.WriteLine("Enter First Name :");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Enter Last Name :");
+            string lastName = Console.ReadLine();
+            Console.WriteLine("Enter Address :");
+            string address = Console.ReadLine();
+            Console.WriteLine("Enter City :");
+            string city = Console.ReadLine();
+            Console.WriteLine("Enter State :");
+            string state = Console.ReadLine();
+            Console.WriteLine("Enter Email :");
+            string email = Console.ReadLine();
+            Console.WriteLine("Enter Zip :");
+            int zip = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Phone Number :");
+            long phoneNumber = Convert.ToInt64(Console.ReadLine());
+            addressBook.AddContact(firstName, lastName, address, city, state, email, zip, phoneNumber);
             addressBook.ViewContact();
         }
     }
